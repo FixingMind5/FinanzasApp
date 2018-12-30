7e865c422750401133e4ff76b08aca2987812515
Yo <fixingmind5@macbook-pro-de-manuel.local>
2018-12-30 12:40:25 -0600

    Initial Commit

---
+++ PlatziFinanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Manuel Alejandro Aguilar Tellez Giron on 12/30/18.
+//  Copyright © 2018 Manuel Alejandro Aguilar Tellez Giron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PlatziFinanzasTests/PlatziFinanzasTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlatziFinanzasTests.swift
+//  PlatziFinanzasTests
+//
+//  Created by Manuel Alejandro Aguilar Tellez Giron on 12/30/18.
+//  Copyright © 2018 Manuel Alejandro Aguilar Tellez Giron. All rights reserved.
+//
+
+import XCTest
+@testable import PlatziFinanzas
+
+class PlatziFinanzasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
